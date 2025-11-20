ed198ff0b830f61928708334fa2ca73b81049d0b
Akash Shinde <akashshinde@AVEGEN-00305.local>
2025-11-20 11:24:28 +0530

    update bridge endpoint to avegen.in for testing purpose

--- app/src/config.js
+++ app/src/config.js
@@ -63,9 +63,9 @@ export default {
   ],
   host: {
     local: "http://localhost:9000",
-    develop: "http://bridgeserver2-uat.eba-tstqu8ae.us-east-1.elasticbeanstalk.com",
-    staging: "http://bridgeserver2-uat.eba-tstqu8ae.us-east-1.elasticbeanstalk.com",
-    production: "https://ws.sagebridge.org"
+      develop: "https://bridge-uat.avegen.in",
+      staging: "https://bridge-uat.avegen.in",
+      production: "https://bridge.avegen.in"
   },
   synapseOauthClients: {
     local: {
